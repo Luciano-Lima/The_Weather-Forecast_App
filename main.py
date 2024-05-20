a5a2788065541f62537d929d630dce0bc7a256a4
Luciano-Lima <ccll4ever1@gmail.com>
2024-05-20 01:08:11 +0100

    Extracting time and limiting the weed day to only 3 letters

--- main.py
+++ main.py
@@ -1,69 +1,65 @@
 from flask import Flask, render_template, request, flash
 import time
-from datetime import datetime, timezone,timedelta
-import requests,pprint
+from datetime import datetime, timezone, timedelta
+import requests, pprint
 import os
 
 from os import path
 if path.exists('env.py'):
     import env
-
+from collections import defaultdict
 
 class WeatherApp:
     def __init__(self):
         self.app = Flask(__name__)
         self.api_key = os.environ.get('API_KEY')
         self.app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
-        self.app.route('/', methods=['GET','POST'])(self.home)
+        self.app.route('/', methods=['GET', 'POST'])(self.home)
 
     def calculate_local_time(self, timestamp, timezone_offset):
         utc_time = datetime.utcfromtimestamp(timestamp)
-        timezone_offset_seconds = timezone_offset
-        local_time = utc_time + timedelta(seconds=timezone_offset_seconds)
-
-        return local_time.strftime('%H:%M')
+        local_time = utc_time + timedelta(seconds=timezone_offset)
+        return local_time.strftime('%Y-%m-%d %H:%M')
 
     def extract_only_time_from_dt_string(self, datetime_string):
-        #Parse the datetime string to a datetime object
         dt_object = datetime.strptime(datetime_string, '%Y-%m-%d %H:%M:%S')
-        #Extract only the time excluding the date
         only_time = dt_object.strftime('%H:%M')
         return only_time
 
+    def group_forecasts_by_day(self, forecasts, timezone_offset):
+        forecasts_by_day = defaultdict(list)
+
+        for forecast in forecasts:
+            local_time = self.calculate_local_time(forecast['dt'], timezone_offset)
+            date, time = local_time.split(' ')
+            day_of_week = datetime.strptime(date, '%Y-%m-%d').strftime('%A')[0:3]
+
+            weather = {
+                'day': day_of_week,
+                "time": time,
+                "temperature": int(forecast['main']['temp']),
+                "icon": forecast['weather'][0]['icon'],
+                "description": forecast['weather'][0]['description'],
+                "humidity": forecast['main']['humidity'],
+                "wind": int(forecast['wind']['speed']),
+                "feels_like": int(forecast['main']['feels_like']),
+                "rain": forecast.get('rain', {}).get('3h', 0),
+                "dt_time": local_time,
+            }
+            forecasts_by_day[day_of_week].append(weather)
+
+        return dict(forecasts_by_day)
 
     def get_weather(self, city):
-        url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={self.api_key}&units=metric&cnt=5'
+        url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={self.api_key}&units=metric'
         response = requests.get(url)
-
+        print(url)
         if response.status_code == 200:
             data = response.json()
-            forecasts = []
 
-            for forecast in data['list']:
-                # local_time = self.calculate_local_time(time.time(),data['city']['timezone'])
-                local_time = self.calculate_local_time(forecast['dt'], data['city']['timezone'])
-                sun_rise = self.calculate_local_time(data['city']['sunrise'],data['city']['timezone'])
-                sun_set = self.calculate_local_time(data['city']['sunset'],data['city']['timezone'])
-                # data_time = self.calculate_local_time(data['city']['dt'])
-                every_3_hours = self.extract_only_time_from_dt_string(forecast['dt_txt'])
+            forecasts_by_day = self.group_forecasts_by_day(data['list'], data['city']['timezone'])
 
-                weather = {
-                    "temperature": int(forecast['main']['temp']),
-                    'icon': forecast['weather'][0]['icon'],
-                    "description": forecast['weather'][0]['description'],
-                    'humidity': forecast['main']['humidity'],
-                    'wind': int(forecast['wind']['speed']),
-                    'country': data['city']['country'],
-                    'feels_like': int(forecast['main']['feels_like']),
-                    # 'data_time': int(forecast['dt']),
-                    'sun_rise': sun_rise,
-                    'sun_set' : sun_set,
-                    'timezone': local_time,
-                    'hours': every_3_hours
-                }
-                forecasts.append(weather)
-
-            return render_template('home.html', forecasts=forecasts, city=city)
+            return render_template('home.html', forecasts_by_day=forecasts_by_day, city=city)
         elif response.status_code == 404:
             flash('City not found. Please enter a valid city name', 'error')
         else:
