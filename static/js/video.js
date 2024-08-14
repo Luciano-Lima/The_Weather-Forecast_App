8be3a1b98401f0f1278212e7a816d8c4a3b73a11
Luciano-Lima <ccll4ever1@gmail.com>
2024-08-14 20:09:17 +0100

    Updadenting background color on weather condition

--- static/js/video.js
+++ static/js/video.js
@@ -3,10 +3,7 @@ class WeatherApp {
 		this.videoEl = document.querySelector('#video_Background video')
 		this.sourceEl = this.videoEl.querySelector('source')
 		this.forescast_descriptionEl = document.getElementById('forescast_description')
-
-		console.log('Video Element:', this.videoEl)
-		console.log('Source Element:', this.sourceEl)
-		console.log('Forecast Description Element:', this.forescastDescriptionEl)
+		this.bg_color = document.getElementById('bg_color')
 	}
 
 	updateVideoBackground() {
@@ -21,13 +18,32 @@ class WeatherApp {
 
 		switch (weatherDescription) {
 			case 'clear sky':
-				videoSource = '../static/video/storm.mp4'
+				videoSource = '../static/video/clear_sky.mp4'
+				this.bg_color.style.backgroundColor = '#426b9b85'
 				break
 			case 'scattered clouds':
-				videoSource = '../static/video/blue_Sky.mov'
+				videoSource = '../static/video/scatted_clouds.mp4'
+				this.bg_color.style.backgroundColor = '#4b647ba6'
+				break
+			case 'overcast clouds':
+				videoSource = '../static/video/overcast_clouds.mp4'
+				this.bg_color.style.backgroundColor = '#89898961'
+				// this.text_slate_50.style.color = '#ffffff'
+				break
+			case 'few clouds':
+				videoSource = '../static/video/few_clouds.mp4'
+				break
+			case 'broken clouds':
+				videoSource = '../static/video/broken_clouds.mp4'
+				this.bg_color.style.backgroundColor = '#60606094'
+				console.log(this.bg_color)
+				break
+			case 'light rain':
+				videoSource = '../static/video/light_rain.mp4'
+				this.bg_color.style.backgroundColor = '#9e9ea282'
 				break
 			default:
-				videoSource = '../static/video/sunset.mp4'
+				videoSource = '../static/video/video_background.mp4'
 		}
 		// Force update the source element's src attribute
 		const uniqueVideoSource = `${videoSource}?t=${new Date().getTime()}`
